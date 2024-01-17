03fe3acc0ce3aa8281163463bc274d4c1bfa5551
DongJinLee <dlehdwls21@naver.com>
2024-01-17 12:37:20 +0900

    :sparkles: square,board 추가

---
+++ src/components/square.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./square.css"
+
+export default class Square extends React.Component{
+    render(){
+        return (
+            <button className="square">
+            </button>
+        )
+    }
+
+}
